e8fe72489a7802c3149ec683da2b15679ed052fd
Yixin Zhang <yizhang@f0r1s7.codam.nl>
2022-11-03 20:25:33 +0100

    ok

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,17 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/02 16:48:01 by yizhang       #+#    #+#                 */
-/*   Updated: 2022/11/03 19:24:19 by yizhang       ########   odam.nl         */
+/*   Updated: 2022/11/03 20:18:56 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "get_next_line.h"
 # include <unistd.h>
 # include <stdlib.h>
+
+# define BUFFER_SIZE 64
 
 typedef struct t_list 
 {
